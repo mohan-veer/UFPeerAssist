9bbf70835cbb3c637846acd4a79abd790e2a55a2
mohan-veer <mohankalyan01@gmail.com>
2025-02-10 10:12:42 -0500

    UFPA-105: changes for password reset and update functionality

--- backend/api/utils/user_util.go
+++ backend/api/utils/user_util.go
@@ -1,6 +1,11 @@
 package utils
 
 import (
+	"crypto/rand"
+	"fmt"
+	"log"
+
+	"github.com/go-gomail/gomail"
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -14,4 +19,28 @@ func HashPassword(password string) (string, error) {
 func CheckPassword(hashedPassword, password string) bool {
 	err := bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
 	return err == nil
+}
+
+// Generate a random 4-digit OTP
+func GenerateOTP() string {
+	b := make([]byte, 3)
+	_, _ = rand.Read(b)
+	return fmt.Sprintf("%04d", (int(b[0])<<16|int(b[1])<<8|int(b[2]))%1000000)
+}
+
+// send OTP
+func SendOTP(email, otp string) error {
+	mailer := gomail.NewMessage()
+	mailer.SetHeader("From", "jamusvenkatesh@gmail.com")
+	mailer.SetHeader("To", email)
+	mailer.SetHeader("Subject", "Your Password reset OTP is ")
+	mailer.SetBody("text/plain", fmt.Sprintf("Your OTP for password reset is: %s. It is valid for 10 minutes.", otp))
+
+	dialer := gomail.NewDialer("smtp.sendgrid.net", 587, "apikey", "")
+
+	err := dialer.DialAndSend(mailer)
+	if err != nil {
+		log.Println("Failed to send OTP through email", err)
+	}
+	return err
 }
