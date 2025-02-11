d68aafc5abb39ecdde2d3d462c0c0318eeade148
subodh <subodhisawake>
2025-02-10 21:22:14 -0500

    Updated pictures

--- frontend/src/components/HeroSection.jsx
+++ frontend/src/components/HeroSection.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import logo from "../assets/images/logo.jpg"; // adjust the path if needed
+import gator from "../assets/images/Florida_Gators_gator_logo.png"
 import "../styles/HeroSection.css";
 
 const HeroSection = () => {
@@ -10,6 +11,9 @@ const HeroSection = () => {
       <nav className="navbar">
         <div className="navbar-logo">
           <img src={logo} alt="UF Peer Assist Logo" />
+        </div>
+        <div className="navbar-logo-right">
+          <img src={gator} alt="UF Peer Assist Logo" />
         </div>
       </nav>
       
--- frontend/src/components/TestimonialsSection.jsx
+++ frontend/src/components/TestimonialsSection.jsx
@@ -7,21 +7,21 @@ const testimonialsData = [
     role: "Undergraduate Student",
     testimonial:
       "UFPeerAssist has been instrumental in connecting me with peers who can help with challenging assignments. The real-time notifications make it feel lively and trustworthy.",
-    image: "https://via.placeholder.com/80",
+    image: "https://www.louisvillecardinal.com/media/2022/11/Depositphotos_122104490_S.jpg",
   },
   {
     name: "Bob Smith",
     role: "Graduate Student",
     testimonial:
       "The platform's secure payment system and seamless collaboration features have made task exchange effortless. It's a lifeline for students here.",
-    image: "https://via.placeholder.com/80",
+    image: "https://media.istockphoto.com/id/1438969575/photo/smiling-young-male-college-student-wearing-headphones-standing-in-a-classroom.jpg?s=612x612&w=0&k=20&c=yNawJP9JGXU6LOL262ME5M1U2xxNKQsvT7F9DZhZCh4=",
   },
   {
     name: "Cathy Brown",
     role: "UF Student",
     testimonial:
       "I love how easy it is to find assistance and offer help on tasks. UFPeerAssist truly understands the student community.",
-    image: "https://via.placeholder.com/80",
+      image: "https://media.istockphoto.com/id/1365601848/photo/portrait-of-a-young-woman-carrying-her-schoolbooks-outside-at-college.jpg?s=612x612&w=0&k=20&c=EVxLUZsL0ueYFF1Nixit6hg-DkiV52ddGw_orw9BSJA=",
   },
 ];
 
